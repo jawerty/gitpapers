fb92e35f452f0ab3b8ec47d3aa302d1eaa54d6d4
jawerty <jawerty210@gmail.com>
2014-05-30 01:48:16 -0400

    changes

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var app = express();
 
 /**take out of file!**/
 GITHUB_ID = process.env.GITHUB_ID
-GITHUB_SECRET = process.env.GITHUB_ID
+GITHUB_SECRET = process.env.GITHUB_SECRET
 /**take out of file**/
 
 app.configure(function(){
